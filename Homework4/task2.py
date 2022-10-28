096e9c7537f833187213e6795b5ad63a6432cd96
Marina <dgoun16@mail.ru>
2022-10-28 21:43:01 +0300

    Добавили ДЗ по семинару 4

--- Homework4/task2.py
+++ Homework4/task2.py
@@ -7,10 +7,9 @@ i = 2
 if n > 1:
     while i <= n:
         if n % i == 0:
-            while n % i == 0:
-                lst.append(i)
-                n //= i
-                i = 2
+            lst.append(i)
+            n //= i
+            i = 2
         else:        
             i += 1
 lst.append(1)
