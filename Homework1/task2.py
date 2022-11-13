f0f313a703b4f9432b08abe244f8f7838cff55ed
Marina <110929816+AnisimovaMN@users.noreply.github.com>
2022-11-13 21:33:59 +0300

    Delete Homework1 directory

--- Homework1/task2.py
+++
@@ -1,9 +0,0 @@
-# 2. Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
-
-o = 1
-for X in range(2):
-    for Y in range(2):
-        for Z in range(2):
-            o *= (not(X or Y or Z) == (not X and not Y and not Z))
-if o == 1:
-    print(True)
